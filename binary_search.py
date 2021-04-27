5f2972c74bffe275570b5fc8e6ea770eda9f8d05
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-04-27 21:44:46 +0545

    iterative

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,18 @@
+#list1=[int(x) for x in input().split()]
+list1=[3,4,5,7,8,9,50]
+t=5
+n=len(list1)
 
+def binar():
+ l=0 ;r=n-1
+ while (l<=r):
+  mid=(r-l)//2 +l
+ 
+  if (t==int(list1[mid])):
+   return mid
+  elif (t>int(list1[mid])):
+   l=mid+1
+  else:
+   r=mid-1
+ return -1
+print(binar())
