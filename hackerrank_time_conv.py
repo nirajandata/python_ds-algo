d658a410f6441e67efd175f597ffc70a0f2a7506
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-05-06 13:49:20 +0545

    added % for calc.

--- hackerrank_time_conv.py
+++ hackerrank_time_conv.py
@@ -1,12 +1,9 @@
 s="01:01:00AM"
 
 def time_conv(s):
-	t=int(s[0:2])
-	if (s[-2:]=="AM"):
-		t=12-t
-	else:
-		t=12+t
+        t=int(s[0:2])%12+(s[-2:]=="PM")*12
 	return "{:02d}".format(t)+":"+s[3:-2]
+	
 		
 	
 		
