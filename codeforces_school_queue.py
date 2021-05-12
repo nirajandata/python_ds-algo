1ece53fd80fce94671f450e6602b73c90bfa7100
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-05-12 11:58:32 +0545

    Create codeforces_school_queue.py

---
+++ codeforces_school_queue.py
@@ -0,0 +1,10 @@
+ad,a=input().split()
+b=list(input())
+d=list(b)
+for j in range(int(a)):
+ for i in range(int(ad)-1):
+  if (b[i]=="B" and b[i+1]=="G"):
+   d[i],d[i+1] =d[i+1],d[i]
+  b=d
+b="".join(b)   
+print(b)
